40b928041b5e42b0be304277d8e15a44074653ad
Fiona Xiong <7125743@qq.com>
2018-08-21 13:37:05 +1200

    update

--- loganalyse/loganalyse.cpp
+++ loganalyse/loganalyse.cpp
@@ -6,6 +6,7 @@
 #include <fstream> 
 #include <string.h>
 #include <sstream>
+#include <Windows.h>
 
 using namespace std;
 
@@ -86,10 +87,11 @@ int main()
 	string sSensorsTime("Sensors time(ms)");
 	string sBailassignTime("Bail processing time(ms)");
 	string bail_num("");
+	string feedanimal = "cow=EMPTY ";
 
 	bool restart = false;  // after reallocation, restart sets to true, the bail_num will be re-counted
 
-	string time11, time00, time;
+	string time11, time00, time, FeedRelayOn[5], FeedRelayOff[5], ExpectedRelayT[5];
 
 	string last_time("00:00:00.000");
 
@@ -107,7 +109,7 @@ int main()
 	output_file << "analyse result by " << filename << std::endl;
 	output_file << '\n' << std::endl;
 	output_file << stime << '\t' << '\t' << sBail << '\t' << '\t' <<
-		sInput00X11 << '\t' << '\t' << sInput00X00 << '\t' << '\t' << sSensorsTime << '\t' << '\t' << sBailassignTime << std::endl;
+		sInput00X11 << '\t' << '\t' << sInput00X00 << '\t' << '\t' << sSensorsTime << '\t' << '\t' << sBailassignTime << '\t' << "FeedorNot" << '\t' << "Head1 Relay On" << '\t' << "Head2 Relay On" << std::endl;
 
 	// read line
 	while (std::getline(file, line))
@@ -117,22 +119,36 @@ int main()
 		string a;
 		int i = 0;
 
-		std::size_t found = line.find("Saber.Shed.Common.Udp.UdpLogListener");
-
+		std::size_t found = line.find("Saber.Shed.Id.Rotary.Mediator.Handlers.UdpLogMessageReceivedHandler");
+		std::size_t foundMCT = line.find("Inputs: MCT");
+		std::size_t foundReallocation = line.find("Reallocating Bails: occurrences");
+		
+		if (foundReallocation != std::string::npos)
+		{
+			output_file << "Reallocation happened" << std::endl;			
+		}
+		
 		if (found == std::string::npos)
 		{
 			continue;
 		}
 
+		if (foundMCT != std::string::npos)
+		{
+			output_file << "MCT happened" << std::endl;
+			continue;
+		}
+				
 		while(iss >> a)
 		{
 			i++;
-			if (i == 1)
+			if (i == 5)
 			{
 				time = a;
 			}
-			else if (i == 5)
+			else if (i == 6)
 			{
+#if 1
 				found = a.find("Inputs:");
 				if (found != std::string::npos)
 				{
@@ -140,20 +156,22 @@ int main()
 					iss >> b;
 					if (b.at(0) == '1')
 					{
-						output_file << time << '\t' << "Full prox" << std::endl;
+						//output_file << time << '\t' << "Full prox" << std::endl;
 					}
 					else if (b.at(1) == '1')
 					{
-						output_file << time << '\t' << "Half prox" << std::endl;
+						//output_file << time << '\t' << "Half prox" << std::endl;
 					}
 
 					if (b.at(3) == '1' && b.at(4) == '1')    // all block
 					{
-						time11 = a.substr(0, 12);
+						//time11 = a.substr(0, 12);
+						time11 = time;
 					}
 					else if (b.at(3) == '0' && b.at(4) == '0')          // all clear
 					{
-						time00 = a.substr(0, 12);
+						//time00 = a.substr(0, 12);
+						time00 = time;
 					}
 					break;
 				}
@@ -163,14 +181,24 @@ int main()
 					found = a.find("assigned");
 					if (found != std::string::npos)
 					{
-						string f;
-						iss >> f;
+						string f, g, h;
+						iss >> f >> g >> h;
 						found = f.find(bail_num);
-						if (found != std::string::npos)
+						if (found != std::string::npos && (strcmp(feedanimal.c_str(), "cow=EMPTY ") == 0 || strcmp(feedanimal.c_str(), "repeat") == 0 || strcmp(feedanimal.c_str(), "") == 0))
 						{
 							output_file << time << '\t' << bail_num << '\t' << '\t' << '\t' << time11 << '\t' << '\t' << time00 << '\t' << '\t' << '\t' << calculat_time(time00, time11)
-								<< '\t' << '\t' << calculat_time(time, last_time) << std::endl;
+								<< '\t' << '\t' << calculat_time(time, last_time) << '\t' << g << ' ' << h << '\t' << feedanimal << std::endl;
 							last_time = time;
+
+							feedanimal = "";
+						}
+						else if (found != std::string::npos)
+						{
+							output_file << time << '\t' << bail_num << '\t' << '\t' << '\t' << time11 << '\t' << '\t' << time00 << '\t' << '\t' << '\t' << calculat_time(time00, time11)
+								<< '\t' << '\t' << calculat_time(time, last_time) << '\t' << g << ' ' << h << '\t' << feedanimal << '\t';
+							last_time = time;
+
+							feedanimal = "";
 						}
 						break;
 					}
@@ -179,12 +207,308 @@ int main()
 				found = a.find("Reverse");
 				if (found != std::string::npos)
 				{
+					if (strcmp(FeedRelayOn[0].c_str(), "") != 0)
+					{
+						output_file << FeedRelayOn[0] << '\t' << FeedRelayOff[0] << '\t' << atoi(FeedRelayOff[0].c_str()) - atoi(FeedRelayOn[0].c_str()) <<  '\t'
+							<< FeedRelayOn[1] << '\t' << FeedRelayOff[1] << '\t' << atoi(FeedRelayOff[1].c_str()) - atoi(FeedRelayOn[1].c_str()) << std::endl;
+						FeedRelayOn[0] = "";
+						FeedRelayOff[0] = "";
+						FeedRelayOn[1] = "";
+						FeedRelayOff[1] = "";
+					}
 					output_file << time << '\t' << "platform reversed" << std::endl;
 					break;
 				}
 
+				found = a.find("Not");
+				if (found != std::string::npos)
+				{
+					string h, i, j;
+					iss >> h >> i >> j;
+					if (strcmp(h.c_str(), "feed") == 0 && strcmp(i.c_str(), "empty") == 0)
+					{
+						feedanimal = "cow=EMPTY ";
+					}
+					break;
+				}
+
+				found = a.find("Feed:");
+				if (found != std::string::npos)
+				{
+					string h, i, j;
+					iss >> h >> i >> j;
+					if (strcmp(h.c_str(), "Not") == 0 && strcmp(i.c_str(), "feed") == 0 && strcmp(j.c_str(), "repeat") == 0)
+					{
+						feedanimal = j;
+					}
+					break;
+				}
+#endif
+				found = a.find("Not");
+				if (found != std::string::npos)
+				{
+					string h, i, j;
+					iss >> h >> i >> j;
+					if (strcmp(h.c_str(), "feed") == 0 && strcmp(i.c_str(), "empty") == 0)
+					{
+						feedanimal = "cow=EMPTY ";
+						output_file << feedanimal << '\t'
+							<< "Empty" << std::endl;
+					}
+					break;
+				}
+
+				found = a.find("Feed:");
+				if (found != std::string::npos)
+				{
+					string h, i, j;
+					iss >> h >> i >> j;
+					if (strcmp(h.c_str(), "Not") == 0 && strcmp(i.c_str(), "feed") == 0 && strcmp(j.c_str(), "repeat") == 0)
+					{
+						feedanimal = j;
+						output_file << feedanimal << '\t'
+							<< "Repeat" << std::endl;
+					}
+					break;
+				}
+
+				found = a.find("Feed");
+				if (found != std::string::npos)
+				{
+					string h, i, j, k;
+					iss >> h >> i >> j >> k;
+					found = h.find("cow=");
+					if (found != std::string::npos)
+					{
+						for (int xj = 0; xj < 5; xj++)
+							FeedRelayOff[xj] = "0";
+
+						if (strcmp(h.c_str(), "cow=UNKNOWN") == 0)
+						{
+							feedanimal = "cow=UNKNOWN ";
+							if (strcmp(i.c_str(), "head[1]") == 0)
+							{
+								string l;
+								iss >> l;
+								if (strcmp(j.c_str(), "off") != 0) {
+									FeedRelayOn[0] = l.substr(1, l.size() - 3);
+									ExpectedRelayT[0] = k.substr(0, k.size() - 2);
+								}
+								else
+								{
+									FeedRelayOn[0] = "0";
+									ExpectedRelayT[0] = "0";
+								}
+							}
+							else if (strcmp(i.c_str(), "head[2]") == 0)
+							{
+								string l;
+								iss >> l;
+								if (strcmp(j.c_str(), "off") != 0) {
+									FeedRelayOn[1] = l.substr(1, l.size() - 3);
+									ExpectedRelayT[1] = k.substr(0, k.size() - 2);
+								}
+								else
+								{
+									FeedRelayOn[1] = "0";
+									ExpectedRelayT[1] = "0";
+								}
+							}
+							else if (strcmp(i.c_str(), "head[3]") == 0)
+							{
+								string l;
+								iss >> l;
+								if (strcmp(j.c_str(), "off") != 0) {
+									FeedRelayOn[2] = l.substr(1, l.size() - 3);
+									ExpectedRelayT[2] = k.substr(0, k.size() - 2);
+								}
+								else
+								{
+									FeedRelayOn[2] = "0";
+									ExpectedRelayT[2] = "0";
+								}
+							}
+							else if (strcmp(i.c_str(), "head[4]") == 0)
+							{
+								string l;
+								iss >> l;
+								if (strcmp(j.c_str(), "off") != 0) {
+									FeedRelayOn[3] = l.substr(1, l.size() - 3);
+									ExpectedRelayT[3] = k.substr(0, k.size() - 2);
+								}
+								else
+								{
+									FeedRelayOn[3] = "0";
+									ExpectedRelayT[3] = "0";
+								}
+							}
+							else if (strcmp(i.c_str(), "head[5]") == 0)
+							{
+								string l;
+								iss >> l;
+								if (strcmp(j.c_str(), "off") != 0) {
+									FeedRelayOn[4] = l.substr(1, l.size() - 3);
+									ExpectedRelayT[4] = k.substr(0, k.size() - 2);
+								}
+								else
+								{
+									FeedRelayOn[4] = "0";
+									ExpectedRelayT[4] = "0";
+								}
+							}
+						}
+						else
+						{
+							feedanimal = h + " " + i;
+							if (strcmp(j.c_str(), "head[1]") == 0)
+							{
+								string l, m;
+								iss >> l >> m;
+								if (strcmp(k.c_str(), "off") != 0)
+								{
+									FeedRelayOn[0] = m.substr(1, m.size() - 3);
+									ExpectedRelayT[0] = l.substr(0, l.size() - 2);
+								}
+								else
+								{
+									FeedRelayOn[0] = "0";
+									ExpectedRelayT[0] = "0";
+								}
+							}
+							else if (strcmp(j.c_str(), "head[2]") == 0)
+							{
+								string l, m;
+								iss >> l >> m;
+								if (strcmp(k.c_str(), "off") != 0) {
+									FeedRelayOn[1] = m.substr(1, m.size() - 3);
+									ExpectedRelayT[1] = l.substr(0, l.size() - 2);
+								}
+								else {
+									FeedRelayOn[1] = "0";
+									ExpectedRelayT[1] = "0";
+								}
+
+							}
+							else if (strcmp(j.c_str(), "head[3]") == 0)
+							{
+								string l, m;
+								iss >> l >> m;
+								if (strcmp(k.c_str(), "off") != 0) {
+									FeedRelayOn[2] = m.substr(1, m.size() - 3);
+									ExpectedRelayT[2] = l.substr(0, l.size() - 2);
+								}
+								else {
+									FeedRelayOn[2] = "0";
+									ExpectedRelayT[2] = "0";
+								}
+							}
+							else if (strcmp(j.c_str(), "head[4]") == 0)
+							{
+								string l, m;
+								iss >> l >> m;
+								if (strcmp(k.c_str(), "off") != 0) {
+									FeedRelayOn[3] = m.substr(1, m.size() - 3);
+									ExpectedRelayT[3] = l.substr(0, l.size() - 2);
+								}
+								else {
+									FeedRelayOn[3] = "0";
+									ExpectedRelayT[3] = "0";
+								}
+							}
+							else if (strcmp(j.c_str(), "head[5]") == 0)
+							{
+								string l, m;
+								iss >> l >> m;
+								if (strcmp(k.c_str(), "off") != 0) {
+									FeedRelayOn[4] = m.substr(1, m.size() - 3);
+									ExpectedRelayT[4] = l.substr(0, l.size() - 2);
+								}
+								else {
+									FeedRelayOn[4] = "0";
+									ExpectedRelayT[4] = "0";
+								}
+							}
+						}
+					}
+					break;
+				}
+
+				found = a.find("feed");
+				if (found != std::string::npos)
+				{
+					string h, i, j, k;
+					iss >> h >> i >> j >> k;
+					bool printornot = true;
+
+					if (strcmp(i.c_str(), "1") == 0)
+					{
+						FeedRelayOff[0] = k.substr(1, k.size() - 3);						
+					}
+					else if (strcmp(i.c_str(), "2") == 0)
+					{
+						FeedRelayOff[1] = k.substr(1, k.size() - 3);						
+					}
+					else if (strcmp(i.c_str(), "3") == 0)
+					{
+						FeedRelayOff[2] = k.substr(1, k.size() - 3);
+						//output_file << FeedRelayOff[1] << '\t' << "(" << atoi(FeedRelayOff[1].c_str()) - atoi(FeedRelayOn[1].c_str()) << ")" << std::endl;
+					}
+					else if (strcmp(i.c_str(), "4") == 0)
+					{
+						FeedRelayOff[3] = k.substr(1, k.size() - 3);
+						//output_file << FeedRelayOff[1] << '\t' << "(" << atoi(FeedRelayOff[1].c_str()) - atoi(FeedRelayOn[1].c_str()) << ")" << std::endl;
+					}
+					else if (strcmp(i.c_str(), "5") == 0)
+					{
+						FeedRelayOff[4] = k.substr(1, k.size() - 3);						
+					}
+
+					for (int xj = 0; xj < 5; xj++)
+					{
+						if (strcmp(ExpectedRelayT[xj].c_str(), "0") != 0 && strcmp(FeedRelayOff[xj].c_str(), "0") == 0)
+						{
+							printornot = false;
+							break;
+						}
+					}
+
+					if (printornot == true)
+					{
+						//unknown cow: one more Tab
+						if (strcmp(feedanimal.c_str(), "cow=UNKNOWN") == 0)
+						{
+							output_file << feedanimal << '\t'<<'\t'
+								<< atoi(ExpectedRelayT[0].c_str()) << '\t'
+								<< atoi(FeedRelayOff[0].c_str()) - atoi(FeedRelayOn[0].c_str()) << '\t'
+								<< atoi(ExpectedRelayT[1].c_str()) << '\t'
+								<< atoi(FeedRelayOff[1].c_str()) - atoi(FeedRelayOn[1].c_str()) << '\t'
+								<< atoi(ExpectedRelayT[2].c_str()) << '\t'
+								<< atoi(FeedRelayOff[2].c_str()) - atoi(FeedRelayOn[2].c_str()) << '\t'
+								<< atoi(ExpectedRelayT[3].c_str()) << '\t'
+								<< atoi(FeedRelayOff[3].c_str()) - atoi(FeedRelayOn[3].c_str()) << '\t'
+								<< atoi(ExpectedRelayT[4].c_str()) << '\t'
+								<< atoi(FeedRelayOff[4].c_str()) - atoi(FeedRelayOn[4].c_str()) << std::endl;
+						}
+						else
+						{
+							output_file << feedanimal << '\t'
+								<< atoi(ExpectedRelayT[0].c_str()) << '\t'
+								<< atoi(FeedRelayOff[0].c_str()) - atoi(FeedRelayOn[0].c_str()) << '\t'
+								<< atoi(ExpectedRelayT[1].c_str()) << '\t'
+								<< atoi(FeedRelayOff[1].c_str()) - atoi(FeedRelayOn[1].c_str()) << '\t'
+								<< atoi(ExpectedRelayT[2].c_str()) << '\t'
+								<< atoi(FeedRelayOff[2].c_str()) - atoi(FeedRelayOn[2].c_str()) << '\t'
+								<< atoi(ExpectedRelayT[3].c_str()) << '\t'
+								<< atoi(FeedRelayOff[3].c_str()) - atoi(FeedRelayOn[3].c_str()) << '\t'
+								<< atoi(ExpectedRelayT[4].c_str()) << '\t'
+								<< atoi(FeedRelayOff[4].c_str()) - atoi(FeedRelayOn[4].c_str()) << std::endl;
+						}
+					}
+				}
+
 			}
-			else if (i == 6)
+#if 1
+			else if (i == 7)
 			{
 				found = a.find("has");
 				if (found != std::string::npos)
@@ -196,9 +520,26 @@ int main()
 						output_file << time << '\t' << "platform stopped" << std::endl;
 						break;
 					}
+					break;
 				}
+
+				found = a.find("FBM");
+				if (found != std::string::npos)
+				{
+					iss >> a;
+					found = a.find("TCP");
+					if (found != std::string::npos && strcmp(FeedRelayOn[0].c_str(), "") != 0)
+					{
+						//output_file << FeedRelayOn[0] << '\t' << FeedRelayOff[0] << '\t' << atoi(FeedRelayOff[0].c_str()) - atoi(FeedRelayOn[0].c_str()) << '\t'
+						//	<< FeedRelayOn[1] << '\t' << FeedRelayOff[1] << '\t' << atoi(FeedRelayOff[1].c_str()) - atoi(FeedRelayOn[1].c_str()) << std::endl;
+						FeedRelayOn[0] = "";
+						FeedRelayOff[0] = "";
+						FeedRelayOn[1] = "";
+						FeedRelayOff[1] = "";
+					}
+				}				
 			}
-			else if (i == 7)
+			else if (i == 8)
 			{
 				if (strcmp(a.c_str(), "Movement") == 0)
 				{
@@ -213,15 +554,8 @@ int main()
 					//output_file << time << '\t' << bail_num << std::endl;
 					break;
 				}
-
-				found = a.find("Reallocating");
-				if (found != std::string::npos)
-				{
-					output_file << time << '\t' << "Reallocation" << std::endl;
-					restart = true;
-					break;
-				}
 			}
+#endif
 		}
 	}
 
